f80c745a6a751c70bf571d8d068f0d7382bb0abb
Gaston Frutos <frutos.gaston@gmail.com>
2023-12-25 16:23:30 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://home-planning-poker-api.herokuapp.com',
-  wsUrl: 'wss://home-planning-poker-api.herokuapp.com',
+  apiUrl: 'https://home-planning-poker-14a375b8dac1.herokuapp.com',
+  wsUrl: 'wss://home-planning-poker-14a375b8dac1.herokuapp.com',
   shareRoomUrl: 'https://home-planning-poker-ui.herokuapp.com/room/'
 };
